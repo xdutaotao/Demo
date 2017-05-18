c8fe585c51b8f3cd051bfab7ce54695b43088eb9
guzhenfu <gzfgeh@sina.com>
2017-05-18 23:35:38 +0800

    add search layout

--- CWorker/app/src/main/java/com/demo/cworker/Fragment/SearchFragment.java
+++ CWorker/app/src/main/java/com/demo/cworker/Fragment/SearchFragment.java
@@ -1,26 +1,56 @@
 package com.demo.cworker.Fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.demo.cworker.Present.SearchPresenter;
 import com.demo.cworker.R;
 import com.demo.cworker.View.SearchView;
-import com.demo.cworker.Present.SearchPresenter;
+import com.gzfgeh.adapter.BaseViewHolder;
+import com.gzfgeh.adapter.RecyclerArrayAdapter;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * create by
  */
-public class SearchFragment extends BaseFragment implements SearchView {
+public class SearchFragment extends BaseFragment implements SearchView, View.OnClickListener {
     private static final String ARG_PARAM1 = "param1";
     @Inject
     SearchPresenter presenter;
+    @BindView(R.id.type_image)
+    ImageView typeImage;
+    @BindView(R.id.edit_text)
+    EditText editText;
+    @BindView(R.id.cancle_action)
+    ImageView cancleAction;
+    @BindView(R.id.search)
+    TextView search;
+    @BindView(R.id.flow_layout)
+    TagFlowLayout flowLayout;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.clear)
+    TextView clear;
     private String mParam1;
+
+    private List<String> list = new ArrayList<>();
+    private RecyclerArrayAdapter<String> adapter;
 
     public static SearchFragment newInstance(String param1) {
         SearchFragment fragment = new SearchFragment();
@@ -45,6 +75,31 @@ public class SearchFragment extends BaseFragment implements SearchView {
         ButterKnife.bind(this, view);
         getActivityComponent().inject(this);
         presenter.attachView(this);
+
+        for(int i=0; i< 10; i++){
+            list.add(i+"");
+        }
+        flowLayout.setAdapter(new TagAdapter<String>(list) {
+            @Override
+            public View getView(FlowLayout parent, int position, String o) {
+                TextView view = (TextView) LayoutInflater.from(parent.getContext()).inflate(android.R.layout.test_list_item, parent);
+                view.setText(list.get(position));
+                return view;
+            }
+        });
+
+
+        adapter = new RecyclerArrayAdapter<String>(getContext(), R.layout.search_item) {
+            @Override
+            protected void convert(BaseViewHolder baseViewHolder, String s) {
+                baseViewHolder.setText(R.id.search_text, s);
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.addAll(list);
+
+        clear.setOnClickListener(this);
+
         return view;
     }
 
@@ -59,4 +114,12 @@ public class SearchFragment extends BaseFragment implements SearchView {
         presenter.detachView();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clear:
+                adapter.clear();
+                break;
+        }
+    }
 }
