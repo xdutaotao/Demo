786f20696b016b930c321d4d04d820c6aac07830
guzhenfu <gzfgeh@sina.com>
2017-05-30 00:11:22 +0800

    add post pic url

---
+++ CWorker/app/src/main/java/com/demo/cworker/Activity/CollectHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.demo.cworker.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.demo.cworker.R;
+
+public class CollectHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_history);
+    }
+}
