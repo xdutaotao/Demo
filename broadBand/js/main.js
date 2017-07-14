7dd330a333fb7aa5e7460bd15a55c3c76a1b68de
guzhenfu <gzfgeh@sina.com>
2017-07-14 18:06:24 +0800

    add file

--- broadBand/js/main.js
+++ broadBand/js/main.js
@@ -21,4 +21,15 @@ $(function(){
     $('#buy').click(function () {
         location.href = 'three.html';
     })
+
+    $("#action").click(function () {
+        layer.open({
+            title: [
+                '购机协议',
+            ],
+            content: '同时存在同一页面移动版和PC版不能同时存在同一页面移动版和PC版不能同时存在同一页面移动版' +
+            '和PC版不能同时存在同一页面移动版和PC版不能同时存在同一页面移动版和PC版不能同时存在同一页面移动版和',
+            btn: '确定'
+        });
+    })
 });
