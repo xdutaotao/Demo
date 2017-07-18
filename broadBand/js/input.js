d81452efbf2ef09f5de4e7127afe4459865df660
guzhenfu <gzfgeh@sina.com>
2017-07-18 21:43:43 +0800

    add file

--- broadBand/js/input.js
+++ broadBand/js/input.js
@@ -27,68 +27,87 @@ Date.prototype.Format = function (fmt) { //author: meizz
 }
 
 $(function(){
+
+    window.onload = function () {
+        Tida.ready({
+            sellerNick:"移动天帅通信专卖店" // 商家名称
+        }, function(){
+
+        })
+    }
+
     var speedList = ['1', '2', '3', '4']
-   $('.speed').change(function () {
-       $('#money').text(speedList[$('.speed option:selected').val()]);
-       data.ex_property.broadband_rate = $('.speed option:selected').text();
-   });
+    $('.speed').change(function () {
+        $('#money').text(speedList[$('.speed option:selected').val()]);
+        data.ex_property.broadband_rate = $('.speed option:selected').text();
+    });
 
     $('.time').change(function () {
         data.ex_property.contract_period = $('.time option:selected').text();
     });
 
+
+
+    var nowDate = new Date();
     var data = {
         ex_property :{
-            broadband_account: "18768144976",
             broadband_rate: "50M",
             contract_period: "1年",
-            effective_date: "2017年11月11号零点",
-            id_number: "430221199107154119",
-            installation_address: "浙江杭州xxxxx",
-            mobile: "18768144976",
-            monthly_fee: "78.89",
-            name: "涂耀",
-            operators: "运营商名称",
-            package_details: "套餐详情描述",
-            package_name: "套餐名称",
-            package_price: "89.89",
-            preferential_info: "9.5折，送200元话费",
+            effective_date: nowDate.getFullYear()+"年"+nowDate.getMonth()+"月"+nowDate.getDay()+"号零点",
+            id_number: $('.personal-input').val(),
+            installation_address: $('.address-input').val(),
+            mobile: $('.phone-input').val(),
+            name: $('.name-input').val(),
+            operators: "移动天帅通信专卖店",
+            package_details: "安装宽带",
+            package_name: "宽带新装",
+            package_price: '1',
+            preferential_info: "宽带优惠",
             province: "浙江",
-            service_type: "broadband",
-            source: "detail"
+            service_type: "broadband"
         },
         detail: {
             mixUserId: GetQueryString("mixUserId"),
             itemId: GetQueryString("itemId"),
             totalPrice: "1",
-            expiryDate: new Date().Format("yyyy-MM-dd HH:mm:ss"),
             pluginInstanceId: GetQueryString("instance_id"),
-            merchantOrderId: '23678698',
+            merchantOrderId: (new Date()).getTime(),
         }
 
     };
 
 
     $('#action').click(function () {
-        // if ($('.name-input').val() == ''){
-        //     toastFunction("户主姓名不能为空");
-        //     return;
-        // }
-        //
-        // if ($('.phone-input').val() == ''){
-        //     toastFunction("电话不能为空");
-        //     return;
-        // }
-        //
-        // if ($('.address-input').val() == ''){
-        //     toastFunction("地址不能为空");
-        //     return;
-        // }
-        //
-        // if ($('.personal-input').val() == ''){
-        //     toastFunction("身份证号不能为空");
-        //     return;
-        // }
+        if ($('.name-input').val() == ''){
+            toastFunction("户主姓名不能为空");
+            return;
+        }
+
+        if ($('.phone-input').val() == ''){
+            toastFunction("电话不能为空");
+            return;
+        }
+
+        if ($('.address-input').val() == ''){
+            toastFunction("地址不能为空");
+            return;
+        }
+
+        if ($('.personal-input').val() == ''){
+            toastFunction("身份证号不能为空");
+            return;
+        }
+
+        if ($('.contact-name-input').val() == ''){
+            toastFunction("地址不能为空");
+            return;
+        }
+
+        if ($('.contact-phone-input').val() == ''){
+            toastFunction("身份证号不能为空");
+            return;
+        }
+
 
         var speed =  $('.speed option:selected').text();
         var time = $('.time option:selected').text();
@@ -105,13 +124,6 @@ $(function(){
             data: data,
             dataType: 'json',
             success: function (data) {
-                //console.log(JSON.stringify(data));
-
-                //$("#agreement").append("88888888888888888");
-
-                if (data.code == '15'){
-                    //console.log(data.msg);
-                }
 
                 params.tradeExToken = data.trade_extend_token;
 
@@ -121,7 +133,7 @@ $(function(){
                     //device 为设备模块，如果需要手机拍照功能，需要填上
                     //提示，请按需加载，加快页面载入速度。
                     //console:1,
-                    module:["customization"], // 需要的模块
+                    module:["customization", 'im'], // 需要的模块
                     debug: 0,
                     combo: 0
                 }, function(e){
@@ -137,6 +149,19 @@ $(function(){
 
         });
 
+    })
+    
+    $('.im').click(function () {
+        var options = {
+            sellerNick:"移动天帅通信专卖店",
+            itemId :GetQueryString("itemId"),
+            shopId : GetQueryString("shopId"),
+            orderId :""
+        };
+
+        Tida.wangwang(options,function(data){
+            alert(JSON.stringify(data));
+        });
     })
 
 });
