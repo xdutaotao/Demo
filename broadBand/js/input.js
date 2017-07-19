bfdff36d7cc5aef7ec903b237bc0eb5b5acc2fd7
guzhenfu <gzfgeh@sina.com>
2017-07-19 17:16:45 +0800

    add file

--- broadBand/js/input.js
+++ broadBand/js/input.js
@@ -19,6 +19,11 @@ $(function(){
         })
     }
 
+    var HEIGHT = $('.main-div').height();
+    $(window).resize(function() {
+        $('.main-div').height(HEIGHT);
+    });
+
     var speedList = ['1', '2', '3', '4']
     $('.speed').change(function () {
         $('#money').text(speedList[$('.speed option:selected').val()]);
@@ -29,7 +34,34 @@ $(function(){
         data.ex_property.contract_period = $('.time option:selected').text();
     });
 
+    var nowDate = new Date();
+    nowDate.setTime(nowDate.getTime() + 24*60*60*1000);
+    var data = {
+        ex_property :{
+            broadband_rate: "50M",
+            contract_period: "1年",
+            effective_date: '',
+            id_number: '',
+            installation_address: '',
+            mobile: '',
+            name: '',
+            operators: "移动天帅通信专卖店",
+            package_details: "安装宽带",
+            package_name: "宽带新装",
+            package_price: '1',
+            preferential_info: "宽带优惠",
+            province: "浙江",
+            service_type: "broadband"
+        },
+        detail: {
+            mixUserId: GetQueryString("mixUserId"),
+            itemId: GetQueryString("itemId"),
+            totalPrice: "1",
+            pluginInstanceId: GetQueryString("instance_id"),
+            merchantOrderId: (new Date()).getTime()
+        }
 
+    };
 
     $('#action').click(function () {
         if ($('.name-input').val() == ''){
@@ -42,13 +74,18 @@ $(function(){
             return;
         }
 
-        if ($('.address-input').val() == ''){
-            toastFunction("地址不能为空");
+        if ($('.phone-input').val().length != 11){
+            toastFunction("手机号码输入错误");
             return;
         }
 
         if ($('.personal-input').val() == ''){
             toastFunction("身份证号不能为空");
+            return;
+        }
+
+        if ($('.personal-input').val().length != 18){
+            toastFunction("身份证号输入错误");
             return;
         }
 
@@ -62,6 +99,16 @@ $(function(){
             return;
         }
 
+        if ($('.contact-phone-input').val().length != 11){
+            toastFunction("联系人手机号码输入错误");
+            return;
+        }
+
+        if ($('.address-input').val() == ''){
+            toastFunction("地址不能为空");
+            return;
+        }
+
 
         var speed =  $('.speed option:selected').text();
         var time = $('.time option:selected').text();
@@ -72,35 +119,16 @@ $(function(){
             tradeToken: GetQueryString("tradeToken")
         };
 
-        var nowDate = new Date();
-        var nextDate = nowDate.setDate(nowDate.getDate() + 1);
-        var data = {
-            ex_property :{
-                broadband_rate: "50M",
-                contract_period: "1年",
-                effective_date: nextDate.getFullYear()+"年"+nextDate.getMonth()+1+"月"+nextDate.getDay()+"号零点",
-                id_number: $('.personal-input').val(),
-                installation_address: $('.address-input').val(),
-                mobile: $('.phone-input').val(),
-                name: $('.name-input').val(),
-                operators: "移动天帅通信专卖店",
-                package_details: "安装宽带",
-                package_name: "宽带新装",
-                package_price: '1',
-                preferential_info: "宽带优惠",
-                province: "浙江",
-                service_type: "broadband"
-            },
-            detail: {
-                mixUserId: GetQueryString("mixUserId"),
-                itemId: GetQueryString("itemId"),
-                totalPrice: "1",
-                pluginInstanceId: GetQueryString("instance_id"),
-                merchantOrderId: (new Date()).getTime()
-            }
-
-        };
-
+        data.ex_property.id_number = $('.personal-input').val();
+        data.ex_property.installation_address = $('.address-input').val();
+        data.ex_property.mobile = $('.phone-input').val();
+        data.ex_property.name = $('.name-input').val();
+        data.ex_property.broadband_rate = $('.speed option:selected').text();
+        data.ex_property.contract_period = $('.time option:selected').text();
+        data.ex_property.effective_date = nowDate.getFullYear()+"年"+(nowDate.getMonth()+1)+"月"+nowDate.getDate()+"号零点";
+        
+        //Tida.showLoading("加载中...");
+        
         $.ajax({
             type: 'POST',
             url: 'https://ydts.ews.m.jaeapp.com/php/trade.php',
@@ -121,6 +149,7 @@ $(function(){
                     combo: 0
                 }, function(e){
                     Tida.customization.buildOrder(params);
+                    //Tida.hideLoading();
                 });
 
             },
@@ -134,6 +163,8 @@ $(function(){
     })
     
     $('.im').click(function () {
+        //Tida.showLoading("加载中...");
+        
         var options = {
             sellerNick:"移动天帅通信专卖店",
             itemId :GetQueryString("itemId"),
@@ -142,7 +173,7 @@ $(function(){
         };
 
         Tida.wangwang(options,function(data){
-
+            //Tida.hideLoading();
         });
     })
 
