400db5a55defd1548179deda18f6ac6161621fed
guzhenfu <gzfgeh@sina.com>
2017-08-10 18:19:58 +0800

    add file

--- VueDemo/HelloVue/src/main.js
+++ VueDemo/HelloVue/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './router/router'
 import store from './store'
+import Mint from 'mint-ui';
 
+Vue.use(Mint);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
--- VueDemo/HelloVue/src/page/msite/msite.vue
+++ VueDemo/HelloVue/src/page/msite/msite.vue
@@ -5,26 +5,35 @@
 <template>
     <div>
         <head-title title="...">
-
             <router-link to="/forget" slot="search" class="searchPic">
                 <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
                     <circle cx="8" cy="8" r="7" stroke="rgb(255,255,255)" stroke-width="1" fill="none"/>
                     <line x1="14" y1="14" x2="20" y2="20" style="stroke:rgb(255,255,255);stroke-width:2"/>
                 </svg>
             </router-link>
-
-
         </head-title>
+
+        <nav class="contained">
+            <mt-swipe class="swipe">
+                <mt-swipe-item class="swipeItem">1</mt-swipe-item>
+                <mt-swipe-item class="swipeItem">2</mt-swipe-item>
+                <mt-swipe-item class="swipeItem">3</mt-swipe-item>
+            </mt-swipe>
+        </nav>
 
     </div>
 </template>
 
 <script>
 	import headTitle from '../../components/header/head'
+    import { Swipe, SwipeItem } from 'mint-ui';
+
 
 	export default {
 		data () {
-			return {}
+			return {
+
+            }
 		},
 		mounted () {
 
@@ -33,7 +42,9 @@
 		computed: {},
 
 		components: {
-			headTitle
+			headTitle,
+            Swipe,
+            SwipeItem
 		},
 		methods: {}
 	}
@@ -46,5 +57,15 @@
         left: .6rem;
         @include wh(1.5rem, 1.5rem);
         @include ct
+    }
+
+    .contained{
+        padding-top: 3rem;
+        @include wh(100%, 60%)
+    }
+
+    .swipeItem{
+        @include wh(100%, 100%);
+        background-color: #3190e8;
     }
 </style>
