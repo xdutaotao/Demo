a2820ae0bad4cf4487795728bd4cc0090648437d
guzhenfu <gzfgeh@sina.com>
2017-09-04 10:20:49 +0800

    add file

--- web/SpringBoot/src/main/java/com/gzfgeh/spring/dao/UserMapper.java
+++ web/SpringBoot/src/main/java/com/gzfgeh/spring/dao/UserMapper.java
@@ -1,11 +1,13 @@
 package com.gzfgeh.spring.dao;
 
 import com.gzfgeh.spring.dao.model.User;
+import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created by guzhenfu on 2017/9/1.
  */
-
 public interface UserMapper {
+    @Select("select * from student where id = #{id}")
     User selectById(String id);
 }
--- web/SpringBoot/src/main/java/com/gzfgeh/spring/service/UserService.java
+++ web/SpringBoot/src/main/java/com/gzfgeh/spring/service/UserService.java
@@ -5,16 +5,18 @@ import com.gzfgeh.spring.dao.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 /**
  * Created by guzhenfu on 2017/9/1.
  */
 @Service
-public class UserService {
+public class UserService implements Serializable{
 
     @Autowired
-    private UserMapper mapper;
+    private UserMapper userMapper;
 
     public User selectById(String id){
-        return mapper.selectById(id);
+        return userMapper.selectById(id);
     }
 }
