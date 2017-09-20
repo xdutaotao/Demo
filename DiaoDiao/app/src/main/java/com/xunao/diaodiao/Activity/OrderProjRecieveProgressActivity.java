f05c24629ee4bd484ffb7c35df578799281f92e9
guzhenfu <gzfgeh@sina.com>
2017-09-20 18:16:16 +0800

    add file

--- DiaoDiao/app/src/main/java/com/xunao/diaodiao/Activity/OrderProjRecieveProgressActivity.java
+++ DiaoDiao/app/src/main/java/com/xunao/diaodiao/Activity/OrderProjRecieveProgressActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -13,6 +14,9 @@ import android.widget.TextView;
 
 import com.gzfgeh.adapter.BaseViewHolder;
 import com.gzfgeh.adapter.RecyclerArrayAdapter;
+import com.lzy.imagepicker.ImagePicker;
+import com.lzy.imagepicker.bean.ImageItem;
+import com.lzy.imagepicker.ui.ImageGridActivity;
 import com.xunao.diaodiao.Bean.MyPublicOddFailReq;
 import com.xunao.diaodiao.Bean.MyPublishOddWorkRes;
 import com.xunao.diaodiao.Present.OrderProjRecieveProgressPresenter;
@@ -21,10 +25,13 @@ import com.xunao.diaodiao.Utils.ToastUtil;
 import com.xunao.diaodiao.Utils.Utils;
 import com.xunao.diaodiao.View.OrderProjRecieveProgressView;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.Observable;
 
 import static com.xunao.diaodiao.Common.Constants.INTENT_KEY;
 
@@ -65,6 +72,12 @@ public class OrderProjRecieveProgressActivity extends BaseActivity implements Or
     private LinearLayoutManager manager;
 
     private RecyclerArrayAdapter<String> signAdapter;
+    private String ADD = "ADD";
+    /**
+     * 图片返回的items
+     */
+    private ArrayList<ImageItem> imageItems = new ArrayList<>();
+    private static final int IMAGE_PICKER = 8888;
 
     public static void startActivity(Context context, int id) {
         Intent intent = new Intent(context, OrderProjRecieveProgressActivity.class);
@@ -130,9 +143,33 @@ public class OrderProjRecieveProgressActivity extends BaseActivity implements Or
         signAdapter = new RecyclerArrayAdapter<String>(this, R.layout.single_image_delete) {
             @Override
             protected void convert(BaseViewHolder baseViewHolder, String s) {
-                baseViewHolder.setImageUrl(R.id.image, s);
+                if (TextUtils.equals(ADD, s)){
+                    baseViewHolder.setVisible(R.id.delete, false);
+                    baseViewHolder.setImageUrl(R.id.image, s);
+                }else{
+                    baseViewHolder.setVisible(R.id.delete, true);
+                    baseViewHolder.setImageUrl(R.id.select_img, s);
+                }
             }
         };
+
+        signAdapter.setOnItemClickListener((view, i) -> {
+            view.findViewById(R.id.delete).setOnClickListener(v -> {
+                imageItems.remove(i);
+                signAdapter.remove(i);
+                if (!signAdapter.getAllData().contains(ADD)) {
+                    signAdapter.add(ADD);
+                }
+            });
+
+            if (TextUtils.equals(signAdapter.getAllData().get(i), ADD)) {
+                selectPhoto();
+            }
+        });
+
+        recyclerViewItem.setAdapter(adapter);
+        post.setVisibility(View.GONE);
+        applyMoney.setVisibility(View.GONE);
 
         post.setOnClickListener(v -> {
 
@@ -147,11 +184,18 @@ public class OrderProjRecieveProgressActivity extends BaseActivity implements Or
 
     }
 
+    private void selectPhoto() {
+        Intent intent = new Intent(this, ImageGridActivity.class);
+        intent.putExtra(ImageGridActivity.EXTRAS_IMAGES, imageItems);
+        startActivityForResult(intent, IMAGE_PICKER);
+    }
+
     @Override
     public void getData(MyPublishOddWorkRes res) {
         if (res.getWork() == null || res.getWork().size() == 0) {
             recyclerViewLayout.setVisibility(View.GONE);
             signLayout.setVisibility(View.VISIBLE);
+            signAdapter.add(ADD);
         } else {
             adapter.addAll(res.getWork());
         }
@@ -170,10 +214,44 @@ public class OrderProjRecieveProgressActivity extends BaseActivity implements Or
         finish();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
+            if (data != null && requestCode == IMAGE_PICKER) {
+                ArrayList<ImageItem> images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                imageItems.clear();
+                imageItems.addAll(images);
+                setResultToAdapter(imageItems);
+            } else {
+                ToastUtil.show("没有数据");
+            }
+        }
+    }
+
+    private void setResultToAdapter(ArrayList<ImageItem> images) {
+        if (images.size() > 0){
+            post.setVisibility(View.VISIBLE);
+            applyMoney.setVisibility(View.VISIBLE);
+        }
+        Observable.from(images)
+                .map(imageItem -> imageItem.path)
+                .toList()
+                .subscribe(strings -> {
+                    signAdapter.clear();
+                    signAdapter.addAll(strings);
+                    if (strings.size() != 10) {
+                        signAdapter.add(ADD);
+                    }
+                });
+    }
+
 
     @Override
     public void onFailure() {
-
+        recyclerViewLayout.setVisibility(View.GONE);
+        signLayout.setVisibility(View.VISIBLE);
+        signAdapter.add(ADD);
     }
 
     @Override
