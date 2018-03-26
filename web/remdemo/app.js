f0d6697d3091bda15d13029b0359d5f8cf13fc2b
guzhenfu <gzfgeh@sina.com>
2018-03-26 22:30:08 +0800

    add file

--- web/remdemo/app.js
+++ web/remdemo/app.js
@@ -1,7 +1,13 @@
-require('./ index.scss');
+require('./index.scss');
 
 let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
 
 let htmlDom = document.getElementsByTagName('html')[0];
 
-htmlDom.style.fontSize = htmlDom / 10 + 'px';
+htmlDom.style.fontSize = htmlWidth / 10 + 'px';
+
+window.addEventListener('resize', e => {
+    let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+
+    htmlDom.style.fontSize = htmlWidth / 10 + 'px';
+})
