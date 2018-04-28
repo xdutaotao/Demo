c67140805204685401de2b97b1e2a5273d4e30ad
gzfgeh <gzfgeh@sina.com>
2018-04-28 11:28:34 +0800

    update file

--- web/VueDemo/TodoListVue/src/TodoList.vue
+++ web/VueDemo/TodoListVue/src/TodoList.vue
@@ -14,7 +14,20 @@
 
     <login></login>
 
-    <label><input type="checkbox"><span></span></label>
+    <!-- <div class="checkboxFive">
+      <input type="checkbox" value="1" id="checkboxOneInput" name="" />
+      <label for="checkboxOneInput"></label>
+  </div> -->
+
+  <div class="checkboxFour">
+      <input type="checkbox" id="checkboxOneInput"  disabled/>
+      <label for="checkboxOneInput"></label>
+  </div>
+
+  <div class="checkboxFour">
+      <input type="checkbox" id="checkboxTwoInput"  checked/>
+      <label for="checkboxTwoInput"></label>
+  </div>
 
 </div>
   
@@ -49,10 +62,92 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+input[type=checkbox] {
+visibility: hidden;
+}
+
+  /* .checkboxFive {
+    width: 25px;
+    margin: 20px 100px;
+    position: relative;
+  }
+
+
+	.checkboxFive label {
+    cursor: pointer;
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    top: 0;
+    left: 0;
+    background: #eee;
+    border:1px solid #ddd;
+}
+
+
+.checkboxFive label:after {
+    opacity: 0.2;
+    content: '';
+    position: absolute;
+    width: 9px;
+    height: 5px;
+    background: transparent;
+    top: 6px;
+    left: 7px;
+    border: 3px solid #333;
+    border-top: none;
+    border-right: none;
+ 
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+}  
+
+.checkboxFive label:hover::after {
+    opacity: 0.5;
+}
+
+.checkboxFive input[type=checkbox]:checked + label:after {
+    opacity: 1;
+} */
 
 
 
+.checkboxFour {
+    width: 20px!important;
+    height: 20px!important;
+    background: #ddd;
+	display: inline-block;
+ 
+    border-radius: 100%;
+    position: relative;
+}
+
+.checkboxFour label {
+    display: block;
+    width: 14px;
+    height: 14px;
+    border-radius: 100px;
+ 
+    -webkit-transition: all .5s ease;
+    -moz-transition: all .5s ease;
+    -o-transition: all .5s ease;
+    -ms-transition: all .5s ease;
+    transition: all .5s ease;
+    cursor: pointer;
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    z-index: 1;
+}
+
+.checkboxFour input[type=checkbox]:checked + label {
+    background: #26ca28;
+}
 
 
 
